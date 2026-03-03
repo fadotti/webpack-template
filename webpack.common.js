991272f8eff61b417539647871f833e08efe53ed
Federico Dotti <fede.dotti.94@gmail.com>
2026-03-03 12:55:27 +0000

    Update first argument of path.resolve

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ export default {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
   plugins: [
